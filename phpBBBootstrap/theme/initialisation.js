89471b169db728ea57352940f6c8ad5a75bbd315
Alexandre Tranchant <Alexandre.Tranchant@gmail.com>
2013-11-17 14:34:38 +0100

    MAJB3.0.2 : Changement de .btn en .btn .btn-default

--- phpBBBootstrap/theme/initialisation.js
+++ phpBBBootstrap/theme/initialisation.js
@@ -26,9 +26,9 @@ function find_username(url)
     $('#sk').addClass('input-medium');
     $('#sd').addClass('input-medium');
     
-    $('a','.pagination-line').addClass('btn btn-mini');
-    $('span.page-dots','.pagination-line').addClass('btn btn-mini disabled');
-    $('strong','.pagination-line').addClass('btn btn-mini disabled');
+    $('a','.pagination-line').addClass('btn btn-default btn-mini');
+    $('span.page-dots','.pagination-line').addClass('btn btn-default btn-mini disabled');
+    $('strong','.pagination-line').addClass('btn btn-default btn-mini disabled');
     //$('strong','.btn').removeClass('btn btn-mini disabled');
     $('span.page-sep','.pagination-line').remove();
     $('img','.forum-avatar-cadre').addClass('img-polaroid');
